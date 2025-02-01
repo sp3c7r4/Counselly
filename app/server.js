5652afc247b4624927720da8041575aef04382cd
Satar Sarafa <sp3c7r40x00@gmail.com>
2025-02-01 18:00:19 +0100

    Edited package json

---
+++ app/server.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { config } from 'dotenv'
+
+config()
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`))
